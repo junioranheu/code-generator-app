c5f6a959c1d0af8b46a780a331d998dced031220
junioranheu <junioranheu@gmail.com>
2024-07-26 20:12:54 -0300

    implement Main

---
+++ CodeGenerator/Main.cs
@@ -0,0 +1,36 @@
+﻿using CodeGenerator.Models;
+using CodeGenerator.Repositories;
+using static CodeGenerator.Utils.Fixtures.Generate;
+using static CodeGenerator.Utils.Fixtures.Get;
+
+namespace CodeGenerator;
+
+public class Main
+{
+    public static string Execute(string solutionName, string contextName, bool isFKGuid, List<Model> models)
+    {
+        string rootPath = GenerateDefaultDirectories(solutionName);
+
+        foreach (var model in models)
+        {
+            List<string> props = GetEntityPropsSplitted(model.Props);
+
+            #region Entity
+            List<Content> entityContent = EntityRepository.GenerateEntity(solutionName, rootPath, className: model.Name, props, isFKGuid);
+            GenerateFiles(contents: entityContent);
+            #endregion
+
+            #region UseCase
+            List<Content> useCaseContent = UseCaseRepository.GenerateUseCaseAndAllItsDependencies(solutionName, contextName, rootPath, useCaseName: model.Name, props, isFKGuid);
+            GenerateFiles(contents: useCaseContent);
+            #endregion
+
+            #region Controller
+            List<Content> controllerContent = ControllerRepository.GenerateController(solutionName, rootPath, className: model.Name, props, isFKGuid);
+            GenerateFiles(contents: controllerContent);
+            #endregion
+        }
+
+        return rootPath;
+    }
+}
--- CodeGenerator/Program.cs
+++ CodeGenerator/Program.cs
@@ -1,7 +1,6 @@
-﻿using CodeGenerator.Enums;
+﻿using CodeGenerator;
+using CodeGenerator.Enums;
 using CodeGenerator.Models;
-using CodeGenerator.Repositories;
-using static CodeGenerator.Utils.Fixtures.Generate;
 using static CodeGenerator.Utils.Fixtures.Get;
 
 #region Input
@@ -16,27 +15,7 @@ List<Model> INPUT_models =
 ];
 #endregion
 
-string rootPath = GenerateDefaultDirectories(INPUT_solutionName);
-
-foreach (var model in INPUT_models)
-{
-    List<string> props = GetEntityPropsSplitted(model.Props);
-
-    #region Entity
-    List<Content> entityContent = EntityRepository.GenerateEntity(INPUT_solutionName, rootPath, className: model.Name, props, INPUT_isFKGuid);
-    GenerateFiles(contents: entityContent);
-    #endregion
-
-    #region UseCase
-    List<Content> useCaseContent = UseCaseRepository.GenerateUseCaseAndAllItsDependencies(INPUT_solutionName, INPUT_contextName, rootPath, useCaseName: model.Name, props, INPUT_isFKGuid);
-    GenerateFiles(contents: useCaseContent);
-    #endregion
-
-    #region Controller
-    List<Content> controllerContent = ControllerRepository.GenerateController(INPUT_solutionName, rootPath, className: model.Name, props, INPUT_isFKGuid);
-    GenerateFiles(contents: controllerContent);
-    #endregion
-}
+string rootPath = Main.Execute(INPUT_solutionName, INPUT_contextName, INPUT_isFKGuid, INPUT_models);
 
 GetLog("Press any key to exit the program", type: LogEnum.Info);
 Console.ReadKey();
