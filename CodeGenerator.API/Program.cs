17a0df92dc08e4b5128ad30163da0eeaa39f0392
junioranheu <junioranheu@gmail.com>
2024-08-02 16:47:03 -0300

    min fix

--- CodeGenerator.API/Program.cs
+++ CodeGenerator.API/Program.cs
@@ -9,11 +9,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 #endregion
