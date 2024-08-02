80f9156e5bd615cfc603540b7b133acba8c7d03b
junioranheu <junioranheu@gmail.com>
2024-08-02 20:10:04 -0300

    min fixes

--- CodeGenerator.API/Program.cs
+++ CodeGenerator.API/Program.cs
@@ -4,21 +4,29 @@ using Main = CodeGenerator.Console.Main;
 #region Program
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 #endregion
 
 app.MapPost("/GenerateCode", (GenerateCodeRequest request) =>
@@ -27,6 +35,11 @@ app.MapPost("/GenerateCode", (GenerateCodeRequest request) =>
     (byte[] bytes, Guid guid) = Main.Execute(request);
 
     return Results.File(bytes, "application/zip", $"{request.SolutionName}{guid}.zip");
+});
+
+app.MapGet("/Teste", () =>
+{
+    return "aweonao";
 });
 
 app.Run();
